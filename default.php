cebe0d6ca0903691b5de8e45c1ff4d5d519e8011
Jack Witt <jwitt1452@gmail.com>
2020-08-22 22:59:44 -0700

    entry points and styles

---
+++ default.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jack Witt</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="styles/main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <h1>Coming Soon...</h1>
+        <!--
+        <img id="b" src="media/bb.png">
+        <script type="text/javascript" src="scripts/app.js"></script>
+        --> 
+    </body>
+</html>
