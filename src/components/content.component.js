6ec1db0055f4c3718eeb28467339b7d6c9f546ab
Jack Witt <jwitt1452@gmail.com>
2020-08-30 17:22:42 -0700

    start

--- src/components/content.component.js
+++ src/components/content.component.js
@@ -4,6 +4,7 @@ import '../styles/partials/_flaticon.scss';
 
 const Content = (props) => {
   return (
+    // TO DO
     <div className="col col-lg-8 col-12 container-fluid content">
       <div className="content-item"></div>
     </div>
