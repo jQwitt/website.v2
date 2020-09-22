61baca1131253c6c85247596cfdfe836e1a60371
Jack Witt <jwitt1452@gmail.com>
2020-09-22 13:22:21 -0700

    initial release

---
+++ build/precache-manifest.94c4d257fb2520a20b874b3843b80123.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b7c69baa45c9177351b41d75a28cb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eba8ddc2162b5877813e",
+    "url": "./static/css/main.aa510f77.chunk.css"
+  },
+  {
+    "revision": "89d252896f4be44114ae",
+    "url": "./static/js/2.63f87db8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.63f87db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba8ddc2162b5877813e",
+    "url": "./static/js/main.81c5e77d.chunk.js"
+  },
+  {
+    "revision": "84e81a77cf43f2640bb0",
+    "url": "./static/js/runtime-main.7dd9548e.js"
+  },
+  {
+    "revision": "2215c93df090b3e0ea054accb8b3852b",
+    "url": "./static/media/Flaticon.2215c93d.svg"
+  },
+  {
+    "revision": "359105a1ed25916230179e5c9c4e6a4a",
+    "url": "./static/media/Flaticon.359105a1.woff"
+  },
+  {
+    "revision": "456ab130ad6252dd1bd8ffc0aa1739cb",
+    "url": "./static/media/Flaticon.456ab130.ttf"
+  },
+  {
+    "revision": "64434042f2918312111e680ca398f044",
+    "url": "./static/media/Flaticon.64434042.woff2"
+  },
+  {
+    "revision": "d85da3d00ba84e324547157b06f15236",
+    "url": "./static/media/Flaticon.d85da3d0.eot"
+  },
+  {
+    "revision": "569e2f36239b0fc9faa000c9bf38ce81",
+    "url": "./static/media/signature.569e2f36.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts('./precache-manifest.6f7e2f88de991e87bbf2d1320ce85d7e.js');
+importScripts(
+  "./precache-manifest.94c4d257fb2520a20b874b3843b80123.js"
+);
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -33,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL('./index.html'),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
